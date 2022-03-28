89c33d16afe13624801c19cd6a37006c181ac2c3
munchosaurus <martinkings1@live.se>
2022-03-28 19:27:48 +0200

    asd

---
+++ My project/Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MovePlatform : MonoBehaviour
+    {
+        private BoxCollider2D collider;
+        private bool turned;
+        private int timer;
+        private Vector2 velocity;
+        private void Update()
+        {
+            if (turned)
+            {
+                timer++;
+                velocity = Vector3.right * 1.5f * Time.deltaTime; 
+            }
+            else
+            {
+                timer--;
+                velocity = Vector3.left * 1.5f * Time.deltaTime; 
+            }
+
+            CheckTurn();
+            transform.position += (Vector3) velocity;
+        }
+
+        void CheckTurn()
+        {
+            if (timer < 0)
+            {
+                turned = true;
+            }
+
+            if (timer > 360)
+            {
+                turned = false;
+            }
+        }
+    }
+}
